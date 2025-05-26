470110f6d8bc9b017b825ffa28f22425c3c062b2
Bui Duc Viet <ducviet@MacBook-Air-cua-Bui.local>
2025-05-26 13:57:43 +0700

    sua lai template

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,14 @@ import DefaultLayout from '../../layouts/Default Layout';
 const Home = () => {
   return (
     <DefaultLayout>
-      <h1>Hello</h1>
+      <div className="container">
+        <h1>Welcome to the ReactJS Template 2024</h1>
+        <p>
+          This template is designed to help you kickstart your React projects
+          with modern best practices.
+        </p>
+        <p>Feel free to explore the code and customize it to fit your needs!</p>
+      </div>
     </DefaultLayout>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    react(), // plugin xử lý React JSX
+    tailwindcss(), // plugin xử lý Tailwind CSS
+  ],
 });
